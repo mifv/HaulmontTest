ecc6e2e90b5e46b6efead1407ddc8a17bb9a8427
DenisKlimov <96593294+mifv@users.noreply.github.com>
2022-01-13 12:33:33 +0400

    StarTest
    StartTest

---
+++ springbootforhaulmont/src/main/java/net/javahaul/springbootforhaulmont/SpringbootforhaulmontApplication.java
@@ -0,0 +1,13 @@
+package net.javahaul.springbootforhaulmont;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootforhaulmontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootforhaulmontApplication.class, args);
+	}
+
+}
---
+++ springbootforhaulmont/src/test/java/net/javahaul/springbootforhaulmont/SpringbootforhaulmontApplicationTests.java
@@ -0,0 +1,13 @@
+package net.javahaul.springbootforhaulmont;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootforhaulmontApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
