fb7f4f1a933173461df0efa90c8cf85adbdc3e0b
DenisKlimov <96593294+mifv@users.noreply.github.com>
2022-01-25 19:17:07 +0400

    finish
    finish

--- springbootforhaulmont/src/main/java/net/javahaul/springbootforhaulmont/SpringbootforhaulmontApplication.java
+++ springbootforhaulmont/src/main/java/net/javahaul/springbootforhaulmont/SpringbootforhaulmontApplication.java
@@ -1,17 +1,13 @@
 package net.javahaul.springbootforhaulmont;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
-@Configuration
-@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class SpringbootforhaulmontApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootforhaulmontApplication.class, args);
     }
-
 }
+
